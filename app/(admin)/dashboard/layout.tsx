bfa7964213fb9232c116d841a0b8a55af370ca33
Hoang Chuong <hoangchuong300494@gmail.com>
2024-06-05 21:52:29 +0700

    add .

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -1,26 +1,26 @@
 import SideNav from '@/components/side-nav';
 import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
 } from '@/components/ui/resizable';
 export default function AdminLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <main className="min-h-screen">
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="min-h-[inherit] md:block"
-      >
-        <ResizablePanel defaultSize={15}>
-          <SideNav />
-        </ResizablePanel>
-        <ResizableHandle withHandle className="w-1 bg-slate-300" />
-        <ResizablePanel defaultSize={85}>{children}</ResizablePanel>
-      </ResizablePanelGroup>
-    </main>
-  );
+    return (
+        <main className="min-h-screen">
+            <ResizablePanelGroup
+                direction="horizontal"
+                className="hidden min-h-[inherit] md:block"
+            >
+                <ResizablePanel defaultSize={10}>
+                    <SideNav />
+                </ResizablePanel>
+                <ResizableHandle withHandle className="w-1 bg-slate-300" />
+                <ResizablePanel defaultSize={90}>{children}</ResizablePanel>
+            </ResizablePanelGroup>
+        </main>
+    );
 }
--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -65,7 +65,6 @@ const SideNav = async () => {
             <div className="white flex h-screen w-full bg-slate-500/50 p-3">
                 <div className="custom-scrollbar flex-1 overflow-y-auto overscroll-contain rounded-3xl bg-gradient-to-r from-indigo-500 to-cyan-950">
                     <div className="grid grid-cols-1 text-white">
-                        <div className="p-12">logo</div>
                         <div className="flex items-center justify-center p-8">
                             <div className="rounded-md bg-slate-300 p-4">
                                 <Home />
