c6ed68e0207a0ce0a2f34e414e088cf4f2a7f6ab
hoang chuong <hoangchuong300494@gmail.com>
2024-06-17 19:11:00 +0700

    add.

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -1,27 +1,27 @@
-import SideNav from '@/components/side-nav';
+import SideNav from '@/components/admin/side-nav';
 import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
 } from '@/components/ui/resizable';
 export default function AdminLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={20}>
-        <SideNav />
-      </ResizablePanel>
-      <ResizableHandle withHandle className="w-1 bg-slate-200/50" />
-      <ResizablePanel defaultSize={80}>
-        <div className="h-full w-full bg-slate-500/50 p-3">
-          <div className="h-full w-full rounded-3xl bg-white p-6">
-            {children}
-          </div>
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
-  );
+    return (
+        <ResizablePanelGroup direction="horizontal">
+            <ResizablePanel defaultSize={20}>
+                <SideNav />
+            </ResizablePanel>
+            <ResizableHandle withHandle className="w-1 bg-slate-200/50" />
+            <ResizablePanel defaultSize={80}>
+                <div className="h-full w-full bg-slate-500/50 p-3">
+                    <div className="h-full w-full rounded-3xl bg-white p-6">
+                        {children}
+                    </div>
+                </div>
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    );
 }
