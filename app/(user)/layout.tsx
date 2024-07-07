b477f3eaa8356071654556c7b963d073d2ee212b
hoang chuong <hoangchuong300494@gmail.com>
2024-07-07 14:04:03 +0700

    add

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,11 @@
 import Header from '@/components/user/header';
 import Footer from '@/components/user/footer';
+import { Viewport } from 'next';
 
+export const viewport: Viewport = {
+    maximumScale: 1,
+    width: 'device-width',
+};
 export default function UserLayout({
     children,
 }: Readonly<{
