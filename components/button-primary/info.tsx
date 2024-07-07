3508fcc657c5459d28221ee2a832aa59077a9517
hoang chuong <hoangchuong300494@gmail.com>
2024-07-07 13:48:25 +0700

    asda

--- components/button-primary/info.tsx
+++ components/button-primary/info.tsx
@@ -43,7 +43,7 @@ const Info = () => {
                 <div className="notiglow"></div>
                 <div className="notiborderglow"></div>
                 <div className="notititle">Chào Thân Ái</div>
-                <div className="notibody font-sans capitalize">
+                <div className="notibody text-sm capitalize">
                     Anh đã thử nghĩ ra mọi cách để bắt chuyện với em, nhưng chắc
                     là anh không quen với lời nói ngọt ngào. Nên anh quyết định
                     viết những dòng này gửi đến em. Tình cờ thấy em, anh thật sự
@@ -57,88 +57,55 @@ const Info = () => {
                     onSubmit={form.handleSubmit(onSubmit)}
                     className="space-y-2"
                 >
-                    <div className="space-y-4">
-                        <FormField
-                            control={form.control}
-                            name="fullName"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Name</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            className="border border-gray-800"
-                                            {...field}
-                                            disabled={isPending}
-                                            placeholder="Enter your name"
-                                            type="text"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="phone"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Phone</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            className="border border-gray-800"
-                                            {...field}
-                                            disabled={isPending}
-                                            placeholder="Enter your phone number"
-                                            type="number"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+                    <FormField
+                        control={form.control}
+                        name="fullName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        className="border border-gray-800"
+                                        {...field}
+                                        disabled={isPending}
+                                        placeholder="Enter your name"
+                                        type="text"
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="phone"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Phone</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        className="border border-gray-800"
+                                        {...field}
+                                        disabled={isPending}
+                                        placeholder="Enter your phone number"
+                                        type="number"
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-                        <div className="mt-8 flex flex-col space-y-2">
-                            <button className="btn-yes" type="submit">
-                                OK
-                            </button>
-                            <button className="btn-no" type="submit">
-                                XÊ RA
-                            </button>
-                        </div>
+                    <div className="mt-8 flex flex-col space-y-2">
+                        <button className="btn-yes" type="submit">
+                            OK
+                        </button>
+                        <button className="btn-no" type="submit">
+                            XÊ RA
+                        </button>
                     </div>
                 </form>
             </Form>
-
-            {/* <form action="">
-                <div className="form__group field">
-                    <input
-                        type="text"
-                        className="form__field"
-                        placeholder="name"
-                    />
-                    <label htmlFor="name" className="form__label">
-                        Name
-                    </label>
-                </div>
-
-                <div className="form__group field">
-                    <input
-                        type="text"
-                        className="form__field"
-                        placeholder="phone number"
-                    />
-                    <label htmlFor="name" className="form__label">
-                        Phone Number
-                    </label>
-                </div>
-
-                <div className="mt-8 flex flex-col space-y-2">
-                    <button className="btn-yes" type="button">
-                        OK
-                    </button>
-                    <button className="btn-no">XÊ RA</button>
-                </div>
-            </form> */}
         </div>
     );
 };
